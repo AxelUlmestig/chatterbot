18b838c53e5b7da79ea8a8bd072f1e87a3167804
Axel Ulmestig <Axel.Ulmestig@gmail.com>
2015-09-27 14:12:41 +0200

    added response to queestions on Watson

---
+++ bot/patterns_dir/watson_info.py
@@ -0,0 +1,14 @@
+import bot.pattern_tools as pt
+
+def match_pattern(tree):
+	if not tree.is_question():
+		return None
+	watson = pt.find_node(tree, pt.match_word("Watson"))
+	if watson:
+		return {"watson": watson.word}
+	return None
+
+def execute_action(knowledge, watson):
+	return "IBM Watson is the solution to all of humanity's problems."
+
+pattern = pt.create_template(match_pattern, execute_action)
