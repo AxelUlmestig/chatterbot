15ab2096850e46e1bb18f06eac3b1fbab6674485
Axel Ulmestig <Axel.Ulmestig@gmail.com>
2015-10-11 21:44:22 +0200

    added understanding of negations in 'adj_noun_pattern'

--- bot/patterns/adj_noun_pattern.py
+++ bot/patterns/adj_noun_pattern.py
@@ -2,14 +2,17 @@ import bot.pattern_tools as pt
 
 def match_pattern(tree):
 	adjective = pt.find_node(tree, pt.match_POS("JJ"))
-	noun = pt.find_node(adjective, pt.match_POS("NNP"))
-	if noun and adjective:
-		return {"noun": noun.word, "adjective": adjective.word}
+	name = pt.find_node(adjective, pt.match_POS("NNP"))
+	if name and adjective:
+		return {"name": name.word, "adjective": adjective.word, "negation": adjective.is_negated()}
 	else:
 		return None
 
-def execute_action(knowledge, noun, adjective):
-	knowledge.add_personal_info(noun, adjective)
+def execute_action(knowledge, name, adjective, negation):
+	if negation:
+		knowledge.remove_personal_info(name, adjective)
+	else:
+		knowledge.add_personal_info(name, adjective)
 	response = "I see."
 	return response
 
