663f6591c2a12ebf47ae99a8bfa7c3e6919be8d2
Axel Ulmestig <Axel.Ulmestig@gmail.com>
2015-10-24 13:34:47 +0200

    added support for dependencies between nouns

--- knowledge/noun.py
+++ knowledge/noun.py
@@ -2,6 +2,8 @@ from knowledge.adjective import Adjective
 
 class Noun:
 
+	noun_relationships = []
+
 	def __init__(self, noun_tree):
 		if not is_noun(noun_tree):
 			raise TypeError
@@ -44,6 +46,28 @@ class Noun:
 				return known_adj
 		return None
 
+	def add_super_noun(self, noun):
+		if noun is self:
+			return False
+		if noun.has_super_noun(self):
+			return False
+		nr = NounRelationship(self, noun)
+		self.noun_relationships.append(nr)
+		return True
+	
+	def get_super_nouns(self):
+		super_nouns = [relationship.super_noun for relationship in self.noun_relationships if relationship.sub_noun is self]
+		return super_nouns
+
+	def has_super_noun(self, noun):
+		super_nouns = self.get_super_nouns()
+		if noun in super_nouns:
+			return True
+		for super_noun in super_nouns:
+			if super_noun.has_super_noun(noun):
+				return True
+		return False
+
 	def __eq__(self, other_noun):
 		if is_noun(other_noun):
 			return other_noun.word == self.word
@@ -51,9 +75,16 @@ class Noun:
 def is_noun(tree):
 	if hasattr(tree, "POS"):
 		return tree.POS[0:2] == "NN"
-	return false
+	return False
 
 def format_adj(adj):
 	if isinstance(adj, Adjective):
 		return adj
 	return Adjective(adj)
+
+class NounRelationship:
+	
+	def __init__(self, sub_noun, super_noun):
+		self.sub_noun = sub_noun
+		self.super_noun = super_noun
+
