f3868a423c56bc7e37495e84baca4801f5de1fc4
Axel Ulmestig <axel.ulmestig@gmail.com>
2015-10-25 14:39:53 +0100

    expanded pattern to bind adjectives to all nouns, not just proper nouns

--- bot/patterns/adj_name_pattern.py
+++ bot/patterns/adj_name_pattern.py
@@ -2,7 +2,7 @@ import bot.pattern_tools as pt
 
 def match_pattern(tree):
 	adjective = pt.find_node(tree, pt.match_POS("JJ"))
-	name = pt.find_node(adjective, pt.match_POS("NNP"))
+	name = pt.find_node(adjective, pt.match_POS("NN", "NNP"))
 	if name and adjective:
 		return {"name": name, "adjective": adjective, "negation": adjective.is_negated()}
 	else:
