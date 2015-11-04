189d3412cb1db2adb1d0a0ac70c64492e76a88ec
Axel Ulmestig <axel.ulmestig@gmail.com>
2015-11-04 17:42:21 +0100

    added initial verb tests

---
+++ test/test_classes/verb_tests.py
@@ -0,0 +1,52 @@
+import unittest
+from watson import text_to_trees
+from knowledge import Noun, Adjective, Verb
+
+verb_str1 = "running quickly"
+verb_str2 = "eating slowly"
+noun_str1 = "a sweet crocodile"
+noun_str2 = "the smelly goblin"
+
+class VerbTests(unittest.TestCase):
+
+	def test_constructor_fail(self):
+		noun_tree = text_to_trees(noun_str1)[0]
+		self.assertRaises(TypeError, Verb, noun_tree)
+
+	def test_constructor_pass(self):
+		verb_tree = text_to_trees(verb_str1)[0]
+		noun = text_to_obj(noun_str1, Noun)
+		try:
+			verb = Verb(verb_tree, noun)
+		except TypeError:
+			self.fail("verb constructor threw an exception when given a verb tree")
+			
+	def test_get_actions_true(self):
+		noun = text_to_obj(noun_str1, Noun)
+		verb = text_to_verb(verb_str1, noun_str1)
+		verb_matches = verb_match = Verb.get_actions(noun)
+		nbr_of_verb_matches = len(verb_matches)
+		expected_nbr = 1
+		self.assertEqual(nbr_of_verb_matches, expected_nbr)
+
+	def test_get_actions_false(self):
+		noun = text_to_obj(noun_str2, Noun)
+		verb = text_to_verb(verb_str1, noun_str1)
+		verb_matches = verb_match = Verb.get_actions(noun)
+		nbr_of_verb_matches = len(verb_matches)
+		expected_nbr = 0
+		self.assertEqual(nbr_of_verb_matches, expected_nbr)
+
+def text_to_obj(text, constructor):
+	tree = text_to_trees(text)[0]
+	return constructor(tree)
+
+def text_to_verb(verb_str, noun_str1, noun_str2=None):
+	noun1 = text_to_obj(noun_str1, Noun)
+	noun2 = None
+	if noun_str2:
+		noun2 = text_to_obj(noun_str2, Noun)
+	verb_tree = text_to_trees(verb_str)[0]
+	return Verb(verb_tree, noun1, noun2)
+
+test_class = VerbTests
