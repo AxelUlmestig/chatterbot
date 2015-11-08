1a1da0a3a260e928bb8f57c78838dd8d475bb6d2
Axel Ulmestig <axel.ulmestig@gmail.com>
2015-11-08 17:10:35 +0100

    added execute_test_class function test_util

--- test/test_util.py
+++ test/test_util.py
@@ -1,5 +1,7 @@
 from watson import text_to_trees
 from knowledge import Noun, Verb
+import unittest
+from concurrencytest import ConcurrentTestSuite, fork_for_tests
 
 def text_to_obj(text, constructor):
 	tree = text_to_trees(text)[0]
@@ -12,3 +14,11 @@ def text_to_verb(text, subj_str, obj_str):
 		noun_obj = text_to_obj(obj_str, Noun)
 	verb_tree = text_to_trees(text)[0]
 	return Verb(verb_tree, noun_sbj, noun_obj)
+
+def execute_test_class(test_class):
+	loader = unittest.TestLoader()
+	suite = unittest.TestSuite()
+	suite.addTests(loader.loadTestsFromTestCase(test_class))
+	runner = unittest.TextTestRunner()
+	concurrent_suite = ConcurrentTestSuite(suite, fork_for_tests(50))
+	runner.run(concurrent_suite)
