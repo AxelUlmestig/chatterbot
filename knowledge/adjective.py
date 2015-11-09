891d3dc7e4426b081f69ad45ecab669091ff3e44
Axel Ulmestig <axel.ulmestig@gmail.com>
2015-11-09 19:41:01 +0100

    adjective.combine now correctly returning True when both Adjectives are the same word

--- knowledge/adjective.py
+++ knowledge/adjective.py
@@ -4,13 +4,14 @@ class Adjective:
 	def __init__(self, tree):
 		if tree.POS != "JJ":
 			raise TypeError("can only create Adjective object from an adjective tree")
-		self.word = tree.word
+		self.word = tree.word.lower()
 		self.is_negated = tree.is_negated()
 		#self.adverb
 
 	def combine(self, other_adj):
 		if self.__eq__(other_adj):
 			self.is_negated = other_adj.is_negated
+			return True
 		return False
 
 	def __str__(self):
