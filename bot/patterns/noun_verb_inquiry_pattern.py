13c49db831d40d68b6e9bd490156a233457c7066
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-03-19 13:25:17 +0100

    added support for "who is <verb>...?" type questions

--- bot/patterns/noun_verb_inquiry_pattern.py
+++ bot/patterns/noun_verb_inquiry_pattern.py
@@ -11,7 +11,20 @@ def match_pattern(tree):
 		return {"verb_tree": verb_tree, "subject_tree": subject_tree, "object_tree": object_tree}
 	return None
 
-def execute_action(knowledge, verb_tree, subject_tree, object_tree):
+def who_inquiry(knowledge, verb_tree, subject_tree, object_tree):
+	negative_response = "No one."
+	if object_tree:
+		obj = knowledge.get_noun(object_tree)
+	matching_verbs = knowledge.get_verbs(verb_tree)
+	if object_tree:
+		obj = knowledge.get_noun(object_tree)
+		matching_verbs = [verb for verb in matching_verbs if verb.get_object() is obj]
+	subjs = [verb.get_subject().word for verb in matching_verbs]
+	if subjs:
+		return format_multiple_answers(subjs)
+	return negative_response 
+
+def specific_inquiry(knowledge, verb_tree, subject_tree, object_tree):
 	positive_respone = "Yes."
 	negative_response = "No."
 	subj = knowledge.get_noun(subject_tree)
@@ -27,5 +40,25 @@ def execute_action(knowledge, verb_tree, subject_tree, object_tree):
 	if len(matches) > 0:
 		return positive_respone
 	return negative_response
+
+def execute_action(knowledge, verb_tree, subject_tree, object_tree):
+	subject_word = subject_tree.word.lower()
+	if subject_word == "who":
+		return who_inquiry(knowledge, verb_tree, subject_tree, object_tree)
+	else:
+		return specific_inquiry(knowledge, verb_tree, subject_tree, object_tree)
+
+def format_multiple_answers(answers):
+	formatted = ""
+	for index, answer in enumerate(answers):
+		formatted += answer
+		if len(answers) - index is 1:
+			formatted += "."
+		elif len(answers) - index is 2:
+			formatted += " and "
+		else:
+			formatted += ", "
+	return formatted
+
 
 pattern = pt.create_pattern(execute_action, match_pattern)
